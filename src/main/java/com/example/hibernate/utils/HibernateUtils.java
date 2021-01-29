b3652d7a4e26d76da32f955a8f8a8f951243c700
s11110101111 <s11110101111@gmail.com>
2021-01-29 18:18:20 +0500

    Create SessionFactory

--- src/main/java/com/example/hibernate/utils/HibernateUtils.java
+++ src/main/java/com/example/hibernate/utils/HibernateUtils.java
@@ -1,21 +1,37 @@
 package com.example.hibernate.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
 import org.hibernate.service.internal.StandardSessionFactoryServiceInitiators;
 
 public class HibernateUtils {
-// begin with SessionFactory
-SessionFactory sessionFactory;
-static {
-    //create static init factory
-    final StandardServiceRegistry registry = new StandardServiceRegistryImpl();
-}
+    // begin with SessionFactory
+    private static SessionFactory sessionFactory;
+
+    static {
+        //create static init factory
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
 
 
-public SessionFactory getSessionFactory(){
-    return sessionFactory;
-}
+    }
+
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
 }
