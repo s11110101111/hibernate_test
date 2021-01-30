44a28accbb5bd0917f992f5e6ac174f5889b836c
s11110101111 <s11110101111@gmail.com>
2021-01-30 09:03:52 +0500

    ok work add entities

--- src/main/java/com/example/hibernate/entities/Department.java
+++ src/main/java/com/example/hibernate/entities/Department.java
@@ -1,12 +1,16 @@
 package com.example.hibernate.entities;
 
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "department_table")
@@ -14,11 +18,54 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column("dept_id")
-    private int departmentId;
+    private Integer departmentId;
     @Column("dept_name")
     private String departmentName;
     @Column("dept_location")
     private String departmentLocation;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "department")
+    //to field into class employee
+    private Set<Employee> employees = new HashSet<Employee>();
 
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
 
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentLocation() {
+        return departmentLocation;
+    }
+
+    public void setDepartmentLocation(String departmentLocation) {
+        this.departmentLocation = departmentLocation;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", departmentLocation='" + departmentLocation + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
 }
--- src/main/java/com/example/hibernate/entities/Employee.java
+++ src/main/java/com/example/hibernate/entities/Employee.java
@@ -1,4 +1,104 @@
 package com.example.hibernate.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "employee_table")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Integer employeeId
+    @Column(name = "employee_name", length = 70, nullable = false)
+    private String employeeName;
+    @Column(name = "employee_date_ofjoin")
+    private Date employeeDateOfJoin;
+    @Column(name = "employee_salary")
+    private Long salary;
+    @Column(name = "employee_bonus")
+    private BigDecimal bonus;
+    @Column(name = "employee_email",length = 50,nullable = false,unique = true)
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "dept_id")
+    private Department department;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public Date getEmployeeDateOfJoin() {
+        return employeeDateOfJoin;
+    }
+
+    public void setEmployeeDateOfJoin(Date employeeDateOfJoin) {
+        this.employeeDateOfJoin = employeeDateOfJoin;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeName='" + employeeName + '\'' +
+                ", employeeDateOfJoin=" + employeeDateOfJoin +
+                ", salary=" + salary +
+                ", bonus=" + bonus +
+                ", email='" + email + '\'' +
+                ", department=" + department +
+                '}';
+    }
 }
